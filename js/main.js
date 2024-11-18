4cd0b2561887be8a75f009c9c66715c3145d3eaf
Dima Bondarenko <dimabrawlstars08@gmail.com>
2024-11-18 21:27:45 +0300

    Adding files

--- js/main.js
+++ js/main.js
@@ -5,6 +5,55 @@ setTimeout(() => {
 function main() {
     addTabheaderFunctionality();
     addCalculateEvents();
+    addTimerForSale();
+    addEventsOnCallUs();
+    window.addEventListener('scroll', scrollEvent);
+}
+
+function scrollEvent() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        const modal = document.querySelector('section.modal');
+        modal.classList.add('visible');
+
+        window.removeEventListener('scroll', scrollEvent);
+        setTimeout(() => {
+            window.addEventListener('scroll',scrollEvent);
+        }, 1000 * 60 * 5); // 5 minutes
+    }
+}
+
+function addEventsOnCallUs() {
+    let butsElements = document.querySelectorAll('button.btn');
+    const buts = Array.from(butsElements).filter(el => el.textContent.includes('Связаться с нами'));
+    toggleClassOnClickToElement(buts, 'section.modal','visible');
+
+    const cross = document.querySelectorAll('.modal__close');
+    toggleClassOnClickToElement(cross, 'section.modal', 'visible');
+}
+
+function addTimerForSale() {
+    const daysElement = document.querySelector('#days');
+    const hoursElement = document.querySelector('#hours');
+    const minutesElement = document.querySelector('#minutes');
+    const secondsElement = document.querySelector('#seconds');
+
+    const timer = setInterval(() => {
+        const remainingTime = getTimeRemaining("2024-12-01");
+
+        if(remainingTime === 0) {
+            daysElement.innerHTML = 0;
+            hoursElement.innerHTML = 0;
+            minutesElement.innerHTML = 0;
+            secondsElement.innerHTML = 0;
+            clearInterval(timer);
+            return;
+        }
+
+        daysElement.innerHTML = remainingTime.days;
+        hoursElement.innerHTML = remainingTime.hours;
+        minutesElement.innerHTML = remainingTime.minutes;
+        secondsElement.innerHTML = remainingTime.seconds;
+    }, 1000);
 }
 
 function addCalculateEvents() {
@@ -15,7 +64,7 @@ function addCalculateEvents() {
     ]
 
     elements.forEach(el => {
-        addClassOnClickEvent(el, 'calculating__choose-item_active');
+        addClickEvent(el, 'calculating__choose-item_active');
     });
 
 
@@ -55,7 +104,7 @@ function addTabheaderFunctionality() {
     });
 }
 
-function addClassOnClickEvent(arr, className) {
+function addClickEvent(arr, className) {
     arr.forEach(el => {
         el.addEventListener('click', () => {
             arr.forEach(e => {
@@ -119,4 +168,40 @@ function calcCalories() { // Calculate Calories
         : 1.725;
     
     return bmr*k;
+}
+
+function getTimeRemaining(endDate) {
+    const now = new Date();
+    const end = new Date(endDate);
+    const timeDifference = end - now;
+
+    if (timeDifference <= 0) {
+        return 0;
+    }
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    return {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+}
+
+/**
+ * Функция для добавления класса с значением className определенному element по нажатию элементов elementsToClick.
+ * @param {HTMLElement} elementsToClick - Элементы к которым нужно добавить event onClick.
+ * @param {HTMLElement} element -Элемент, которому нужно добавить определенный класс
+ * @param {className} className -Имя класса
+ */
+function toggleClassOnClickToElement(elementsToClick, element, className) {
+    elementsToClick.forEach(el => {
+        el.addEventListener('click', () => {
+            document.querySelector(element).classList.toggle(className);
+        });
+    });
 }
